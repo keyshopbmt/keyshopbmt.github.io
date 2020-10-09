6cb1f4d694ec39a4379a84f0cf7f2bc9dbf4e43d
lthoang <lthoang@apcs.vn>
2020-10-09 17:15:42 +0800

    Updates

---
+++ precache-manifest.fb33c0d5002dedb231962936ade5dede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e470748f02a7483f62cba125ebf545e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9399ad71928d4b6ddfde",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "adffec18253448dc85f4",
+    "url": "/static/js/2.99a674ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99a674ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9399ad71928d4b6ddfde",
+    "url": "/static/js/main.678438e0.chunk.js"
+  },
+  {
+    "revision": "291d7992bef03360500c",
+    "url": "/static/js/runtime-main.4e9f6bd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
