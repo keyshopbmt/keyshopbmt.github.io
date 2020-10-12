bd799e64fb6b9d9406e67e2b8c77902a6985fc42
Hoang Le <lthoang@apcs.vn>
2020-10-12 12:52:21 +0800

    Add sample code getting data from google spreadsheet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useEffect, useState } from 'react';
+import Tabletop from 'tabletop';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: '16MevfF7ba_NvMveIFMy8xd1zGeNqCa0yJY3Zfo-fvsE',
+      simpleSheet: true
+    }).then((data) => setData(data))
+      .catch((err) => console.warn(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((item, i) => (
+        <Fragment key={i}>
+          <p>Tên sản phẩm -- {item.name}</p>
+        </Fragment>))
+      }
     </div>
   );
 }
