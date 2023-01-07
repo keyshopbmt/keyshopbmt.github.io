d569b6c1e69b2ce3c3b1909ee63a67cb355e9073
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-01-07 15:56:09 +0700

    thirdth commit

--- src/component/papes/Home/Home.jsx
+++ src/component/papes/Home/Home.jsx
@@ -6,6 +6,24 @@ export default function Home() {
   return (
     <div className="home">
     <Navbar/>
+   
+
+            <div className="card text-bg-dark" id="thongdiep">
+                <img src="../assets/slider_bg1.jpg" className="card-img" alt="..." />
+                <div className="card-img-overlay">
+                    <div className="text-civic">
+                        <h3 className="card-title">DỊCH VỤ KHẨN CẤP: 0942975959</h3>
+                        <h2 className="text-card">DỊCH VỤ LUÔN SẴN SÀNG 24/7</h2>
+
+                        <p className="card-text">
+                            Tách mình khỏi những chuẩn mực cố định, Honda Civic chuyển mình để bứt phá trong từng đường
+                            nét <br />
+                            thiết kế và tính năng, kiến tạo nên những thành công mới cho chủ sở hữu
+                        </p>
+                    </div>
+                </div>
+            </div>
+
    </div>
   );
 }
--- src/component/papes/Navbar.jsx
+++ src/component/papes/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-
+import { Link } from "react-router-dom";
 import { MDBIcon } from "mdb-react-ui-kit";
 
 export default function Navbar() {
@@ -48,116 +48,126 @@ export default function Navbar() {
         <div className="container">
           <div className="middle_bar_inner">
             <div className="logo">
-              <a href="index.html">
+
+              <a >
                 <img src="assets/logo1.png" alt="logo" />
               </a>
             </div>
-
-            <div className="header_right_part">
-              <div className="mainnav">
-                <ul className="main_menu">
-                  <li className="menu-item menu-item-has-children active">
-                    <a href="/">Home</a>
+            <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm ">
+        <div className="container">
+          
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse fs-3 " id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Trang chủ
+                </a>
+              </li>
+              
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  to="./following"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  Sản Phẩm
+                </Link>
+                <ul className="dropdown-menu">
+                <li>
+                    <a className="dropdown-item"  href="./following/civicr">
+                      Honda Civic Type R
+                    </a>
                   </li>
-                  <li className="menu-item">
-                    <a href="about.html">About</a>
+                  <li>
+                    <a className="dropdown-item"  href="./following/accord">
+                      Honda Accrod
+                    </a>
                   </li>
-                  <li className="menu-item menu-item-has-children">
-                    <a href="services.html">Services</a>
-                    <ul className="sub-menu">
-                      <li className="menu-item">
-                        <a href="service-details.html">Service Details</a>
-                      </li>
-                    </ul>
+                  <li>
+                    <a className="dropdown-item" href="./following/civic">
+                      Honda Civic
+                    </a>
                   </li>
-                  <li className="menu-item menu-item-has-children">
-                    <a href="#">Blog</a>
-                    <ul className="sub-menu">
-                      <li className="menu-item">
-                        <a href="blog-1.html">Blog One</a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="blog-2.html">Blog Two</a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="blog-details.html">Blog Details</a>
-                      </li>
-                    </ul>
+                  <li>
+                    <a className="dropdown-item" href="./following/crv">
+                      Honda Crv
+                    </a>
                   </li>
-                  <li className="menu-item menu-item-has-children">
-                    <a href="#">Pages</a>
-                    <ul className="sub-menu">
-                      <li className="menu-item">
-                        <a href="team.html">Team</a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="faq.html">FAQ</a>
-                      </li>
-                    </ul>
+                  
+                  <li>
+                    <a className="dropdown-item" href="./following/hrv">
+                      Honda Hrv
+                    </a>
                   </li>
-                  <li className="menu-item menu-item-has-children">
-                    <a href="#">Contact</a>
-                    <ul className="sub-menu">
-                      <li className="menu-item">
-                        <a href="contact-1.html">Contact One</a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="contact-2.html">Contact Two</a>
-                      </li>
-                    </ul>
+                  <li>
+                    <a className="dropdown-item" href="./following/city">
+                      Honda City
+                    </a>
                   </li>
                 </ul>
-                <ul data-ma5order="ma5-ul" class="ma5menu__panel--active lvl-0">
-									<li data-ma5order="ma5-li-1"><span class="ma5menu__btn--enter"></span><a href="index.html">Home</a>
-										
-									</li>
-									<li data-ma5order="ma5-li-2"><a href="about.html">About</a></li>
-									<li data-ma5order="ma5-li-3"><span class="ma5menu__btn--enter"></span><a href="services.html">Services</a>
-										<ul data-ma5order="ma5-ul-3" class="lvl-1">
-											<li data-ma5order="ma5-li-3-1"><div class="ma5menu__leave"><span class="ma5menu__btn--leave"></span>Services</div><a href="service-details.html">Service Details</a></li>
-										</ul>
-									</li>
-									<li data-ma5order="ma5-li-4"><span class="ma5menu__btn--enter"></span><a href="#">Blog</a>
-										<ul data-ma5order="ma5-ul-4" class="lvl-1">
-											<li data-ma5order="ma5-li-4-1"><div class="ma5menu__leave"><span class="ma5menu__btn--leave"></span>Blog</div><a href="blog-1.html">Blog One</a></li>
-											
-										</ul>
-									</li>
-									<li data-ma5order="ma5-li-5"><span class="ma5menu__btn--enter"></span><a href="#">Pages</a>
-										<ul data-ma5order="ma5-ul-5" class="lvl-1">
-											<li data-ma5order="ma5-li-5-1"><div class="ma5menu__leave"><span class="ma5menu__btn--leave"></span>Pages</div><a href="team.html">Team</a></li>
-											<li data-ma5order="ma5-li-5-2"><a href="faq.html">FAQ</a></li>
-										</ul>
-									</li>
-									<li data-ma5order="ma5-li-6"><span class="ma5menu__btn--enter"></span><a href="#">Contact</a>
-										<ul data-ma5order="ma5-ul-6" class="lvl-1">
-											<li data-ma5order="ma5-li-6-1"><div class="ma5menu__leave"><span class="ma5menu__btn--leave"></span>Contact</div><a href="contact-1.html">Contact One</a></li>
-											
-										</ul>
-									</li>
-								</ul>
-              </div>
-              <div className="header_search">
-                <form className="search_form" action="search.php">
-                  <input
-                    type="text"
-                    name="search"
-                    className="keyword form-control"
-                    placeholder="Search..."
-                  />
-                  <button type="submit" className="form-control-submit">
-                    <i className="ion-ios-search"></i>
-                  </button>
-                </form>
-              </div>
-
-              <div className="aside_open">
-                <i className="ion-ios-keypad"></i>
-              </div>
-              <button className="ma5menu__toggle" type="button">
-                <i className="ion-android-menu"></i>
-              </button>
-            </div>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  to="./following"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  Dịch vụ
+                </Link>
+                <ul className="dropdown-menu">
+                <li>
+                    <a className="dropdown-item"  href="./">
+                      Dịch vụ 1
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item"  href="./">
+                    Dịch vụ 1
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="./">
+                    Dịch vụ 1
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="./">
+                    Dịch vụ 1
+                    </a>
+                  </li>
+                  
+                
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Tin Tức
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Liên Hệ</a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+           
           </div>
         </div>
       </div>
