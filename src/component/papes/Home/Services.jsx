db40095a91c68f245beb630e430a96fb89a156a6
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-01-08 16:19:37 +0700

    fifth commit

--- src/component/papes/Home/Services.jsx
+++ src/component/papes/Home/Services.jsx
@@ -5,7 +5,7 @@ export default function Services() {
   return (
     <div>
         <div className="services">
-				<div className="row">
+				<div className="row g-0 ">
 					<div className="col-lg-4 col-md-6 pd_0">
 						<div className="iconbox3 first">
 							<div className="iconbox_wrapper">
@@ -13,14 +13,14 @@ export default function Services() {
 									<div className="iconbox_icon">
 										<img className="first_icon" src="assets/services/icon12.png" alt="icon"/>
 										<img className="second_icon" src="assets/services/icon12_1.png" alt="icon"/>
-                    ::after
+               
 									</div>
 								</div>
 								<div className="iconbox_content">
-									<h3><a href="service-details.html">Household Security Device Installation</a></h3>
+									<h3><a href="/">Household Security Device Installation</a></h3>
 									<p>We offering you comprehensive services to the industrial, commercial, residential &amp; automotive sectors. </p>
 									<div className="read_more">
-										<a href="service-details.html"><span>Learn More</span></a>
+										<a href="/"><span>CHI TIẾT</span></a>
 									</div>
 								</div>
 							</div>
@@ -36,10 +36,10 @@ export default function Services() {
 									</div>
 								</div>
 								<div className="iconbox_content">
-									<h3><a href="service-details.html">Automobile Door lock Opening</a></h3>
+									<h3><a href="/">Automobile Door lock Opening</a></h3>
 									<p>We will provide you and your company with professional, prompt, efficient and competitive service.</p>
 									<div className="read_more">
-										<a href="service-details.html"><span>Learn More</span></a>
+										<a href="/"><span>CHI TIẾT</span></a>
 									</div>
 								</div>
 							</div>
@@ -55,10 +55,10 @@ export default function Services() {
 									</div>
 								</div>
 								<div className="iconbox_content">
-									<h3><a href="service-details.html">Openning Locked House Door</a></h3>
+									<h3><a href="/">Openning Locked House Door</a></h3>
 									<p>Our team include the trained locksmiths experienced architectural hardware specialists &amp; lock experts.  </p>
 									<div className="read_more">
-										<a href="service-details.html"><span>Learn More</span></a>
+										<a href="/"><span>CHI TIẾT</span></a>
 									</div>
 								</div>
 							</div>
