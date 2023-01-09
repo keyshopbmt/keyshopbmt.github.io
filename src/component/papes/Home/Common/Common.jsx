b82d9038ec87cdf1ba4b6f10550cb2dbe136c19b
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-01-09 22:19:20 +0700

    seventh commit

--- src/component/papes/Home/Common/Common.jsx
+++ src/component/papes/Home/Common/Common.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './common.css'
+import './common.scss'
 
 export default function Common() {
   return (
@@ -8,8 +8,8 @@ export default function Common() {
                 <div class="container">
 					<div class="section_header text-center aos-init aos-animate" data-aos="fade-up">
 						<div class="shadow_icon"><img src="assets/services/shadow_icon2.png" alt=""/></div>
-						<h6 class="section_sub_title">Common problem solutions</h6>
-						<h1 class="section_title">Most popular locksmith services</h1>
+						<h6 class="section_sub_title">Các Sự Cố Thường Gặp</h6>
+						<h1 class="section_title">Các Dịch Vụ Sửa Khóa Phổ Biến Nhất</h1>
 						<p class="section_desc">When a locksmith is discussing re-keying that are referring<br/> 
 							to altering the code of the lock so that it </p>
 					</div>
@@ -21,9 +21,9 @@ export default function Common() {
                                         <img src="assets/services/v_1.jpg" alt="img"/>
                                     </div>
                                     <div class="iconbox_content">
-                                        <h3><a href="service-details.html">Automobile Door lock Opening</a></h3>
+                                        <h3><a href="/" style={{ textDecoration: 'none', color:'#dc3545' }}>Mở Khóa Cửa Ôtô</a></h3>
                                         <div class="read_more">
-											<a href="service-details.html"><span>Get Service</span></a>
+											<a href="/" style={{ textDecoration: 'none' }}><span style={{color:'white'}} >Nhận Dịch Vụ</span></a>
 										</div>
 									</div>
                                 </div>
@@ -36,9 +36,9 @@ export default function Common() {
                                         <img src="assets/services/v_2.jpg" alt="img"/>
                                     </div>
                                     <div class="iconbox_content">
-                                        <h3><a href="service-details.html">Auto door lock installation</a></h3>
+                                        <h3><a href="/" style={{ textDecoration: 'none',color:'#dc3545' }}>Dịch Vụ Mở Khóa Tại Nhà</a></h3>
                                         <div class="read_more">
-											<a href="service-details.html"><span>Get Service</span></a>
+											<a href="/" style={{ textDecoration: 'none', }}><span style={{color:'white'}}>Nhận Dịch Vụ</span></a>
 										</div>
 									</div>
                                 </div>
@@ -51,18 +51,39 @@ export default function Common() {
                                         <img src="assets/services/v_3.jpg" alt="img"/>
                                     </div>
                                     <div class="iconbox_content">
-                                        <h3><a href="service-details.html">Lost lock key Duplication</a></h3>
+                                        <h3><a href="/" style={{ textDecoration: 'none',color:'#dc3545' }}>Làm Lại Chìa Khóa Bị Mất</a></h3>
                                         <div class="read_more">
-											<a href="service-details.html"><span>Get Service</span></a>
+											<a href="/" style={{ textDecoration: 'none' }}><span style={{color:'white'}}>Nhận Dịch Vụ</span></a>
 										</div>
 									</div>
                                 </div>
                             </div>
                         </div>
 					</div>
-					<h6 class="explore_all">If You Have Other Locksmith Problems <a href="services.html">Explore All Services</a> or Call to <span>+33 123 456 789</span> </h6>
+					<h6 class="explore_all">Nếu Bạn Gặp Các Vấn Đề Về Khóa,<a href="/" style={{ textDecoration: 'none', textAlign:'center' }}>Hãy Khám Phá Các Dịch Vụ</a> Hoặc Gọi Tới <span style={{color:'red'}}>0942975959 (Mr.Tiến)</span> </h6>
 				</div>
 			</div>
+            <div class="banner type_3 aos-init aos-animate" data-aos="fade-up">
+				<div class="container">
+					<div class="banner_content">
+						<div class="row">
+							<div class="col-lg-7">
+								<div class="banner_text">
+									<h1>Nếu Bạn Có Bất Kỳ Câu Hỏi Nào Khác, Bạn Có Thể Gọi Cho Chúng Tôi
+                                    </h1>
+								</div>
+							</div>
+							<div class="col-lg-5">
+								<div class="banner_phone">
+									<img src="assets/phone3.png" alt=""/>
+									<span> 0942975959 </span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
     </div>
   )
 }
