55b1ce20abb0f12b26df505a43d81097907c1ea1
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-07 20:54:13 +0700

    key car content commit

--- src/component/papes/Dichvu/Carkey/Carkey.jsx
+++ src/component/papes/Dichvu/Carkey/Carkey.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
 import Navbar from "../../Navbar/Navbar";
 import "./carkey.css";
 
@@ -25,11 +25,14 @@ export default function Carkey() {
       </div>
     <div className="title-car">
       <h2>Dịch vụ sửa chữa và thay thế chìa khóa Ôtô tại Key Shop Bmt</h2>
-      <MDBRow>
-      <MDBCol md='8'>
+
+
+        <div className="row-title">
       <h4>
-        Dịch vụ sửa chữa và thay thế khóa Ôtô 24/24
+        Dịch vụ sửa chữa và thay thế khóa Ôtô 24/24 :
+        <span>0942975959</span>
       </h4>
+      <p>(chúng tôi làm chìa khóa Ôtô từ xa cho bất kì chiếc xe nào)</p>
       <ul>
         <li>Dịch vụ cắt chìa khóa nhanh chóng uy tín.</li>
         <li>Giá cả phải chăng.</li>
@@ -37,15 +40,25 @@ export default function Carkey() {
         <li>Thay thế và sữa chữa chìa khóa Ôtô chuyên nghiệp</li>
 
       </ul>
-
-      </MDBCol>
-      <MDBCol md='4'>
-        <h4> 0942975959
-        </h4>
-        <p>
-Chúng tôi làm chìa khóa ô tô từ xa cho bất kỳ chiếc xe nào.</p>
-      </MDBCol>
-    </MDBRow>
+<h6>**Nhận báo giá cho dịch vụ cắt chìa khóa của bạn ngay hôm nay</h6>
+  <img src="../assets/services/carkey/1920.jpg" alt="" className="car-img" />
+    </div> 
+    </div>
+    <div className="car-key">
+    <div className="carkey-title">
+      <h3>Làm chìa khóa Ôtô dễ dàng</h3>
+      <p>Chúng tôi cung cấp nhiều loại dịch vụ làm chìa khóa Ôtô , nếu bạn bị mất tất cả chìa khóa Ôtô, chúng tôi có thể khôi phục bộ chìa khóa bị mất, flash thiết bị cố định của bạn và xóa thông tin khỏi bộ nhớ của những chiếc bị mất.</p>
+      <p>Nếu bạn mất quyền truy cập vào phương tiện của mình và không thể đến cửa hàng của chúng tôi, chỉ cần gọi cho chúng tôi. Chúng tôi cung cấp dịch vụ di động. Nhân viên điều phối sẽ nhanh chóng cử chuyên gia của chúng tôi đến và anh ta sẽ nhanh chóng khôi phục quyền truy cập vào xe bằng cách làm một bộ chìa khóa ngay tại chỗ.</p>
+      <button type="button" class="btn btn-secondary btn-lg btn-block">Cần chìa khóa xe mới ? Hãy gọi cho chúng tôi 0942975959 </button>
+      <h3>Tiết kiệm chi phí làm chìa khóa xe của bạn</h3>
+      <p>Nhiều người đổ xô đến đại lý địa phương của họ để nhận dịch vụ thay thế chìa khóa Ôtô. Cuối cùng, họ thường phải trả rất nhiều tiền cho chìa khóa xe mới của mình. Thật không may, họ không biết rằng có những lựa chọn thay thế rẻ hơn có thể tiết kiệm thời gian và tiền bạc mà không ảnh hưởng đến chất lượng của chìa khóa thông minh. Một trong những lựa chọn thay thế là gọi cho chúng tôi, thợ khóa xe hơi tại địa phương của bạn.</p>
+      <b>Dịch vụ thợ khóa Ôtô đôi khi có thể tiết kiệm tới 50% chi phí chìa khóa Ôtô của bạn.</b>
+      <p>Giả sử bạn bị mất chìa khóa Ôtô hoặc cần sao chép lại. Trong trường hợp đó, chúng tôi cung cấp dịch vụ thay thế chìa khóa Ôtô. Chúng tôi có thể cung cấp chìa khóa Ôtô mới với giá cả rất phải chăng.</p>
+      <p>Điều tuyệt vời nhất là bạn thậm chí không cần phải lái xe hoặc kéo xe của mình đến cửa hàng của chúng tôi.</p>
+    <div className="image-keycar">
+      <img src="../assets/services/carkey/key.jpg" alt="" />
+    </div>
+    </div>
     </div>
     </div>
   );
