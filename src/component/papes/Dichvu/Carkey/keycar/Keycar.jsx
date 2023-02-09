31b7fdc7dc612a999795e47e7c8474ccd487c5e2
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-09 20:53:48 +0700

    Key car content commit vs css

---
+++ src/component/papes/Dichvu/Carkey/keycar/Keycar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Keycar() {
+  return (
+    
+    <div className="car-key">
+    <img
+      src="../assets/services/carkey/key.jpg"
+      alt=""
+      className="rounded-4 shadow-4 float-end m-4"
+      style={{ width: "350px", height: "500px", paddingLeft: "10px" }}
+    />
+    <div className="carkey-title">
+      <h3>Làm chìa khóa Ôtô dễ dàng</h3>
+      <p>
+        Chúng tôi cung cấp nhiều loại dịch vụ làm chìa khóa Ôtô , nếu bạn bị
+        mất tất cả chìa khóa Ôtô, chúng tôi có thể khôi phục bộ chìa khóa bị
+        mất, flash thiết bị cố định của bạn và xóa thông tin khỏi bộ nhớ của
+        những chiếc bị mất.
+      </p>
+      <p>
+        Nếu bạn mất quyền truy cập vào phương tiện của mình và không thể đến
+        cửa hàng của chúng tôi, chỉ cần gọi cho chúng tôi. Chúng tôi cung
+        cấp dịch vụ di động. Nhân viên điều phối sẽ nhanh chóng cử chuyên
+        gia của chúng tôi đến và anh ta sẽ nhanh chóng khôi phục quyền truy
+        cập vào xe bằng cách làm một bộ chìa khóa ngay tại chỗ.
+      </p>
+      <button className="btn btn-secondary btn-lg btn-block">
+        Cần chìa khóa xe mới ? Hãy gọi ngay cho chúng tôi:{" "}
+        <span> 0942975959</span>
+      </button>
+      <h3>Tiết kiệm chi phí làm chìa khóa xe của bạn</h3>
+      <p>
+        Nhiều người đổ xô đến đại lý địa phương của họ để nhận dịch vụ thay
+        thế chìa khóa Ôtô. Cuối cùng, họ thường phải trả rất nhiều tiền cho
+        chìa khóa xe mới của mình. Thật không may, họ không biết rằng có
+        những lựa chọn thay thế rẻ hơn có thể tiết kiệm thời gian và tiền
+        bạc mà không ảnh hưởng đến chất lượng của chìa khóa thông minh. Một
+        trong những lựa chọn thay thế là gọi cho chúng tôi, thợ khóa xe hơi
+        tại Đắk Lắk.
+      </p>
+      <b>
+        Dịch vụ thợ khóa Ôtô đôi khi có thể tiết kiệm tới 50% chi phí chìa
+        khóa Ôtô của bạn.
+      </b>
+      <p>
+        Giả sử bạn bị mất chìa khóa Ôtô hoặc cần sao chép lại. Trong trường
+        hợp đó, chúng tôi cung cấp dịch vụ thay thế chìa khóa Ôtô. Chúng tôi
+        có thể cung cấp chìa khóa Ôtô mới với giá cả rất phải chăng.
+      </p>
+      <p>
+        Điều tuyệt vời nhất là bạn thậm chí không cần phải lái xe hoặc kéo
+        xe của mình đến cửa hàng của chúng tôi.
+      </p>
+    </div>
+  </div>
+  )
+}
