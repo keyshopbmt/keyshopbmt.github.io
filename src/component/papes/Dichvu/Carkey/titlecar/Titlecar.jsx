e7d33e5c91cc2769f67500d71407aeeb84ef353f
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-09 20:54:23 +0700

    title car key content vs css commit

---
+++ src/component/papes/Dichvu/Carkey/titlecar/Titlecar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './titlecar.css';
+
+export default function Titlecar() {
+  return (
+    <div className="title-car">
+        <h2>Dịch vụ sửa chữa và thay thế chìa khóa Ôtô tại Key Shop Bmt</h2>
+
+        <div className="row-title">
+          <h4>
+            Dịch vụ sửa chữa và thay thế khóa Ôtô 24/24 :<span>0942975959</span>
+          </h4>
+          <p>(chúng tôi làm chìa khóa Ôtô từ xa cho bất kì chiếc xe nào)</p>
+          <ul>
+            <li>Dịch vụ cắt chìa khóa nhanh chóng uy tín.</li>
+            <li>Giá cả phải chăng.</li>
+            <li>Cung cấp tất cả các dịch vụ và giải pháp về chìa khóa Ôtô.</li>
+            <li>Thay thế và sữa chữa chìa khóa Ôtô chuyên nghiệp</li>
+          </ul>
+          <h6>**Nhận báo giá cho dịch vụ cắt chìa khóa của bạn ngay hôm nay</h6>
+          <img
+            src="../assets/services/carkey/1920.jpg"
+            alt=""
+            className="car-img"
+          />
+        </div>
+      </div>
+  )
+}
