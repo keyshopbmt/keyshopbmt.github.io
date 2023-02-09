f5563139a84410792535d840247d5e3473325fec
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-09 20:52:54 +0700

    easystep commit nội dung

---
+++ src/component/papes/Dichvu/Carkey/easystep/Easystep.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+export default function Easystep() {
+  return (
+    <div className="easy-step">
+    <h2>4 Bước đơn giản để thay thế chìa khóa xe Ôtô của bạn</h2>
+    <Container className="step-image">
+      <Row md={4}>
+        <Col>
+          <img
+            src="../assets/services/carkey/no-1.png"
+            alt=""
+            className="image-no1"
+          />
+          <br />
+          <img
+            src="../assets/services/carkey/call-us.png"
+            alt=""
+            className="image-call"
+          />
+          <h4>Liên hệ với chúng tôi:</h4>
+          <h4>
+            <span>0942975959</span>
+          </h4>
+        </Col>
+        <Col>
+          <img
+            src="../assets/services/carkey/no-2.png"
+            alt=""
+            className="image-no1"
+          />
+          <br />
+          <img
+            src="../assets/services/carkey/technician.png"
+            alt=""
+            className="image-call"
+          />
+          <h4>Một kỹ thuật viên sẽ được cử đến</h4>
+        </Col>
+        <Col>
+          <img
+            src="../assets/services/carkey/no-3.png"
+            alt=""
+            className="image-no1"
+          />
+          <br />
+          <img
+            src="../assets/services/carkey/car-key.png"
+            alt=""
+            className="image-call"
+          />
+          <h4>Kỹ thuật viên sẽ thay chìa khóa Ôtô cho bạn </h4>
+        </Col>
+        <Col>
+          <img
+            src="../assets/services/carkey/no-4.png"
+            alt=""
+            className="image-no1"
+          />
+          <br />
+          <img
+            src="../assets/services/carkey/payment.png"
+            alt=""
+            className="image-call"
+          />
+          <h4>Bạn thanh toán dịch vụ và nhận hóa đơn bảo hành</h4>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+  )
+}
