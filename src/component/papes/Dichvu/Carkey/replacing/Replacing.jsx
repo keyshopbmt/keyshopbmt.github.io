cb504ba9a33f299e98d196adb506a6b691c03a6b
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-10 16:06:43 +0700

    css vs content replacing commit

---
+++ src/component/papes/Dichvu/Carkey/replacing/Replacing.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './replacing.css';
+
+export default function Replacing() {
+  return (
+    <div className="replacing-carkey">
+    <img
+      src="../assets/services/carkey/key-cutting.jpg"
+      alt=""
+      className="rounded-4 shadow-4 float-start m-2"
+      style={{ width: "350px", height: "300px" ,paddingRight:"20px"}}
+    />
+    <h2><b>Thay chìa khóa Ôtô ngày càng khó</b></h2>
+    <div className="replacing-key">
+      <h3><b>
+
+        Thợ khóa phải đầu tư vào các thiết bị mới để luôn phù hợp với sự
+        thay đổi công nghệ
+      </b>
+      </h3>
+      <p>
+        Với những tiến bộ không ngừng trong công nghệ bảo mật Ôtô, các
+        loại hệ thống chìa khóa từ xa mới đang được giới thiệu hàng năm
+        với nhiều tùy chọn và khả năng hơn.
+      </p>
+      <p>
+        Ngày nay, hầu hết tất cả các nhà sản xuất Ôtô đều cài đặt một số
+        loại tính năng ra vào không cần chìa khóa trong xe của họ khi
+        chúng được sản xuất. Những chiếc Ôtô được trang bị công nghệ tiên
+        tiến nhất thậm chí còn có các ứng dụng cho phép chủ sở hữu mở hoặc
+        khóa Ôtô của bạn từ xa hoặc truy cập thông tin trực tuyến về Ôtô
+        của bạn.
+      </p>
+      <p>
+        Hệ thống từ xa Ôtô như vậy an toàn hơn và phức tạp hơn để lập
+        trình. Một thợ khóa Ôtô phải đối mặt với những thách thức khi việc
+        lập trình trở nên khó khăn hơn mỗi năm. Thợ khóa Ôtô phải theo kịp
+        xu hướng và mua các công cụ lập trình tốt hơn với khả năng lập
+        trình lớn hơn để luôn phù hợp. Nó dẫn đến việc dịch vụ cắt chìa
+        khóa Ôtô trở nên đắt đỏ hơn.
+      </p>
+      <h3><b>
+
+        Kỹ thuật viên của chúng tôi được trang bị các kĩ thuật mới nhất
+      </b>
+      </h3>
+      <p>
+        Đây là lý do tại sao chúng tôi tại Key Shop BMT đầu tư mạnh vào
+        công nghệ và thiết bị mới để cung cấp dịch vụ thay thế chìa khóa ô
+        tô cho nhiều loại phương tiện. Nếu cần thay chìa khóa từ xa, bạn
+        có thể tin tưởng vào nhóm của chúng tôi tại Key Shop BMT để cung
+        cấp cho bạn dịch vụ thay thế chìa khóa ô tô chuyên nghiệp.
+      </p>
+    </div>
+  </div>
+  )
+}
