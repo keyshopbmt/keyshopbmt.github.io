479b256c8e1c8d1c843c80c8bdd1fcc589be46a3
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-15 20:51:03 +0700

    procedure moto key commit

---
+++ src/component/papes/Dichvu/Motokey/proceduremoto/Proceduremoto.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+
+export default function Proceduremoto() {
+  return (
+    <div className='procedure-motokey'>
+            <h2>Các bước làm chìa khóa xe máy</h2>
+<div className='procedure-moto'>
+          
+      <img
+        src="../assets/services/moto/writing_icon.png"
+        alt=""
+        className="rounded-4 shadow-4 float-start m-4"
+        style={{
+          width: "50px",
+          height: "50px",
+          
+        }}
+      />
+
+      <div className="procedure-keymoto">
+        <h3><b>Chuẩn bị thông tin về xe của bạn và gọi cho chúng tôi.</b></h3>
+        <p>
+          <span>
+          Kỹ thuật viên sẽ phải xác minh quyền sở hữu của chiếc xe máy khi đến nơi.Đây là một bước mà tất cả các thợ khóa phải tuân theo. Có sẵn những thông tin này sẽ tiết kiệm thời gian và cho phép kỹ thuật viên thợ khóa bắt đầu làm việc với xe máy nhanh hơn.
+          </span>
+        </p>
+      </div>
+      <div className="procedure-moto">
+        <img
+          src="../assets/services/moto/mechanical_chain-1920w.png"
+          alt=""
+          className="rounded-4 shadow-4 float-start m-4"
+          style={{
+            width: "50px",
+            height: "50px",
+            
+          }}
+        />
+        <div className="procedure-keymoto">
+          <h3><b>
+
+          
+          </b>
+          </h3>
+          <p>
+            <span>
+              Dựa trên thông tin bạn cung cấp, kỹ thuật viên sẽ xác định
+              remote có phù hợp với xe của bạn và chuẩn bị cho công việc.
+              Khi kỹ thuật viên đến địa điểm của bạn, anh ta sẽ ngay lập
+              tức tiến hành thay thế chìa khóa mới cho Ôtô của bạn.
+            </span>
+          </p>
+        </div>
+        <div className="procedure-moto">
+          <img
+            src="../assets/services/moto/machine_cutter_icon-1920w.png"
+            alt=""
+            className="rounded-4 shadow-4 float-start m-4"
+            style={{
+              width: "50px",
+              height: "50px",
+            
+            }}
+          />
+          <div className="procedure-keymoto">
+            <h3><b>
+              
+              Bước hoàn thiện
+            </b>
+              </h3>
+            <p>
+              <span>
+                Bước cuối cùng, tùy thuộc vào loại chìa khóa remote mà bạn
+                có. Nếu loại remote bạn có không phải là loại chìa khóa
+                thông thường, kỹ thuật viên sẽ phải lấy "mã khóa" cơ khí
+                của bạn và cắt lưỡi dao. Lưỡi dao có thể là lưỡi cắt tiêu
+                chuẩn hoặc lưỡi cắt laser. Tất cả các kỹ thuật viên của
+                chúng tôi đều được trang bị các công cụ phù hợp và có thể
+                cắt cả hai loại một cách dễ dàng.
+              </span>
+            </p>
+            <p>
+              <span >
+                Sau khi lưỡi dao được cắt, công việc sẽ hoàn thành và bạn
+                sẽ có một chìa khóa dự phòng mới cho Ôtô của mình!
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="procedure-moto">
+          <img
+            src="../assets/services/moto/racing_finish_flag.png"
+            alt=""
+            className="rounded-4 shadow-4 float-start m-4"
+            style={{
+              width: "50px",
+              height: "50px",
+            
+            }}
+          />
+          <div className="procedure-keymoto">
+            <h3><b>
+              
+              Bước hoàn thiện
+            </b>
+              </h3>
+            <p>
+              <span>
+                Bước cuối cùng, tùy thuộc vào loại chìa khóa remote mà bạn
+                có. Nếu loại remote bạn có không phải là loại chìa khóa
+                thông thường, kỹ thuật viên sẽ phải lấy "mã khóa" cơ khí
+                của bạn và cắt lưỡi dao. Lưỡi dao có thể là lưỡi cắt tiêu
+                chuẩn hoặc lưỡi cắt laser. Tất cả các kỹ thuật viên của
+                chúng tôi đều được trang bị các công cụ phù hợp và có thể
+                cắt cả hai loại một cách dễ dàng.
+              </span>
+            </p>
+            <p>
+              <span >
+                Sau khi lưỡi dao được cắt, công việc sẽ hoàn thành và bạn
+                sẽ có một chìa khóa dự phòng mới cho Ôtô của mình!
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>   
+    </div>
+    
+  )
+}
