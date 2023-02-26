dc9309dc2112e368d54968eb69b8ba48e48926cb
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-26 20:33:00 +0700

    router contact commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import Home from "../component/papes/Home/Home";
-import News from "../component/papes/News/News";
+
 import Dichvu from "../component/papes/Dichvu/services/Dichvu";
 import Carkey from "../component/papes/Dichvu/Carkey/Carkey";
 import Motokey from "../component/papes/Dichvu/Motokey/Motokey";
+import Contact from "../component/papes/Contact/Contact";
 
 
 
@@ -11,7 +12,7 @@ import Motokey from "../component/papes/Dichvu/Motokey/Motokey";
 const publicRoutes =[
     {path: '/', component: Home},
     {path: '/dichvu', component: Dichvu},
-    {path: '/news', component: News},
+    {path: '/contact', component: Contact},
     {path: '/carkey', component: Carkey},
     {path: '/motokey', component: Motokey}
  
