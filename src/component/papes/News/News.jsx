2f384dcd0e750f698b230d21d7f5e0304ffb5842
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-02-28 20:53:07 +0700

    fanpage commit

--- src/component/papes/News/News.jsx
+++ src/component/papes/News/News.jsx
@@ -1,23 +1,85 @@
-import React from "react";
-import Navbar from "../Navbar/Navbar";
+import React, {useState} from "react";
+
 import "./news.css";
 
-export default function News() {
-  return (
-    <div>
-      <Navbar />
 
-      <div className="iframe-fb-container">
-        <iframe
-        className="iframe-fb border"
-          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftienphatexpertkeyworld&tabs=timeline&width=2000&height=420&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
-      
+export default function News() {
+
+  return (
+    <div >
+      <div
+   
+        id="sos_fb_slider"
+        className="soslider_class soslider_inner "
+       style={{ width: "300px",
+       height: "500px",
+       zIndex: "1000",
+       background: "none 0px 0px repeat scroll rgb(59, 89, 152)",
+       borderRadius: "0px",
+       padding: "5px 0px 0px 5px",
+       top: "62.5px",
+       right: "-305px"
+       
+      }}
+      >
+        <div  >
           
-          scrolling="no"
-          frameborder="0"
-          allowfullscreen="true"
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-        ></iframe>
+
+          <img
+            src="../assets/9_right.png"
+           
+            style={{
+              display: "block",
+              position: "absolute",
+              left: "-24px",
+              top: "208px",
+              cursor: "pointer",
+              
+            }}
+            
+          />
+        </div>
+
+        <div
+          id="sos_fb_slider_inner"
+          className="soslider_inner"
+          style={{
+            width: "295px",
+            height: "490px",
+            padding: "5px 0px 5px 5px",
+            background: "none 0px 0px repeat scroll rgb(255, 255, 255)",
+            borderRadius: "0px",
+            overflow: "hidden",
+          }}
+        >
+          <div
+            
+          >
+       
+              <iframe
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftienphatexpertkeyworld&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId"
+                width="340"
+                height="500"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameborder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+              ></iframe>
+          
+          </div>
+          <div
+            id="sos-fbw"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "40%",
+              display: "none",
+            }}
+          >
+            Loading...
+          </div>
+        </div>
       </div>
     </div>
   );
