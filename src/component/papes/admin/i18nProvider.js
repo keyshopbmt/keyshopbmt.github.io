d2cb7e1f5db5773c1058840ad1ea8d32e3de9f0e
Hoc Lap Trinh <vtmbmt11231111@gmail.com>
2023-07-19 21:29:38 +0700

    i18n commit

--- src/component/papes/admin/i18nProvider.js
+++ src/component/papes/admin/i18nProvider.js
@@ -1,14 +1,12 @@
 import polyglotI18nProvider from "ra-i18n-polyglot";
-import en from "ra-language-english";
 import vi from "ra-language-vietnamese";
 
-const translations = { en, vi };
+const translations = { vi };
 
 export const i18nProvider = polyglotI18nProvider(
   (locale) => translations[locale],
-  "en", // default locale
+  "vi", // default locale
   [
-    { locale: "en", name: "English" },
     { locale: "vi", name: "Vietnamese" },
   ],
   { allowMissing: true }
